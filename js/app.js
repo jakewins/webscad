459849bb6efdc29193827a931a0211e9fc8a72de
Jacob Hansson <jacob@voltvoodoo.com>
2012-12-24 23:22:39 -0600

    Added a demo page

---
+++ js/app.js
@@ -0,0 +1,162 @@
+// Just a quick throw-together mash of code right now, 
+// to be modified into a proper demo further on.
+
+CSG.prototype.setColor = function(r, g, b) {
+  this.toPolygons().map(function(polygon) {
+    polygon.shared = [r, g, b];
+  });
+};
+
+// Convert from CSG solid to GL.Mesh object
+CSG.prototype.toMesh = function() {
+  var csg = this.canonicalized();
+  var mesh = new GL.Mesh({ normals: true, colors: true });
+  var vertexTag2Index = {};
+  var vertices = [];
+  var colors = [];
+  var triangles = [];
+  // set to true if we want to use interpolated vertex normals
+  // this creates nice round spheres but does not represent the shape of
+  // the actual model
+  var smoothlighting = false;   
+  var polygons = csg.toPolygons();
+  var numpolygons = polygons.length;
+  for(var polygonindex = 0; polygonindex < numpolygons; polygonindex++)
+  {
+    var polygon = polygons[polygonindex];
+    var color = [0,0,1];
+    if(polygon.shared && polygon.shared.color)
+    {
+      color = polygon.shared.color;
+    }
+    var indices = polygon.vertices.map(function(vertex) {
+      var vertextag = vertex.getTag();
+      var vertexindex;
+      if(smoothlighting && (vertextag in vertexTag2Index))
+      {
+        vertexindex = vertexTag2Index[vertextag];
+      }
+      else
+      {
+        vertexindex = vertices.length;
+        vertexTag2Index[vertextag] = vertexindex;
+        vertices.push([vertex.pos.x, vertex.pos.y, vertex.pos.z]);
+        colors.push(color);
+      }
+      return vertexindex;
+    });
+    for (var i = 2; i < indices.length; i++) {
+      triangles.push([indices[0], indices[i - 1], indices[i]]);
+    }
+  }
+  mesh.triangles = triangles;
+  mesh.vertices = vertices;
+  mesh.colors = colors;
+  mesh.computeWireframe();
+  mesh.computeNormals();
+  return mesh;
+};
+
+var angleX = 20;
+var angleY = 20;
+var viewers = [];
+
+// Set to true so lines don't use the depth buffer
+Viewer.lineOverlay = false;
+
+// A viewer is a WebGL canvas that lets the user view a mesh. The user can
+// tumble it around by dragging the mouse.
+function Viewer(csg, width, height, depth) {
+viewers.push(this);
+
+// Get a new WebGL canvas
+var gl = GL.create();
+this.gl = gl;
+this.mesh = csg.toMesh();
+
+// Set up the viewport
+gl.canvas.width = width;
+gl.canvas.height = height;
+gl.viewport(0, 0, width, height);
+gl.matrixMode(gl.PROJECTION);
+gl.loadIdentity();
+gl.perspective(45, width / height, 0.1, 100);
+gl.matrixMode(gl.MODELVIEW);
+
+// Set up WebGL state
+gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+gl.clearColor(0.93, 0.93, 0.93, 1);
+gl.enable(gl.DEPTH_TEST);
+gl.enable(gl.CULL_FACE);
+gl.polygonOffset(1, 1);
+
+// Black shader for wireframe
+this.blackShader = new GL.Shader('\
+  void main() {\
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;\
+  }\
+  ', '\
+  void main() {\
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);\
+  }\
+  ');
+
+// Shader with diffuse and specular lighting
+this.lightingShader = new GL.Shader('\
+  varying vec3 color;\
+  varying vec3 normal;\
+  varying vec3 light;\
+  void main() {\
+    const vec3 lightDir = vec3(1.0, 2.0, 3.0) / 3.741657386773941;\
+    light = (gl_ModelViewMatrix * vec4(lightDir, 0.0)).xyz;\
+    color = gl_Color.rgb;\
+    normal = gl_NormalMatrix * gl_Normal;\
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;\
+  }\
+  ', '\
+  varying vec3 color;\
+  varying vec3 normal;\
+  varying vec3 light;\
+  void main() {\
+    vec3 n = normalize(normal);\
+    float diffuse = max(0.0, dot(light, n));\
+    float specular = pow(max(0.0, -reflect(light, n).z), 32.0) * sqrt(diffuse);\
+    gl_FragColor = vec4(mix(color * (0.3 + 0.7 * diffuse), vec3(1.0), specular), 1.0);\
+  }\
+  ');
+
+gl.onmousemove = function(e) {
+  if (e.dragging) {
+    angleY += e.deltaX * 2;
+    angleX += e.deltaY * 2;
+    angleX = Math.max(-90, Math.min(90, angleX));
+
+    viewers.map(function(viewer) {
+      viewer.gl.ondraw();
+    });
+  }
+};
+
+var that = this;
+gl.ondraw = function() {
+  gl.makeCurrent();
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.loadIdentity();
+  gl.translate(0, 0, -depth);
+  gl.rotate(angleX, 1, 0, 0);
+  gl.rotate(angleY, 0, 1, 0);
+
+  if (!Viewer.lineOverlay) gl.enable(gl.POLYGON_OFFSET_FILL);
+  that.lightingShader.draw(that.mesh, gl.TRIANGLES);
+  if (!Viewer.lineOverlay) gl.disable(gl.POLYGON_OFFSET_FILL);
+
+  if (Viewer.lineOverlay) gl.disable(gl.DEPTH_TEST);
+  gl.enable(gl.BLEND);
+  that.blackShader.draw(that.mesh, gl.LINES);
+  gl.disable(gl.BLEND);
+  if (Viewer.lineOverlay) gl.enable(gl.DEPTH_TEST);
+  };
+
+  gl.ondraw();
+}
